584b656f585b74ff126a4efedbc4a07b36b4714d
Lucas <lucaschmain@gmail.com>
2023-02-21 19:14:07 -0300

    coding swagger documentation api

--- dist/src/Utils/MotorcycleCategory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MotorcycleCategory;
-(function (MotorcycleCategory) {
-    MotorcycleCategory["Street"] = "Street";
-    MotorcycleCategory["Custom"] = "Custom";
-    MotorcycleCategory["Trail"] = "Trail";
-})(MotorcycleCategory || (MotorcycleCategory = {}));
-exports.default = MotorcycleCategory;
